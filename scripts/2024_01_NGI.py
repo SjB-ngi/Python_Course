aa95456f764bd86bd6d38b15c6065e4cff4e99bb
Sjur Beyer <sjur.beyer@ngi.no>
2024-11-24 20:12:02 +0100

    Initial commit, added first session script file

---
+++ scripts/2024_01_NGI.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Script for the "Python basics for geoscience and geotechnics (Pilot course)"
+from the Norwegian Geotechnical Institute. The course is held in November to
+December 2024 in 4x4 hour sessions.
+
+This script contains the code that was written during the first session
+The code is for educational purposes only.
+All content of the repository falls under the MIT-license -> see license file.
+
+Author: Dr. Georg H. Erharter, georg.erharter@ngi.no
+Modifications: Sjur Beyer, sjur.beyer@ngi.no
+"""
+
+###########################
+# session 1 on 25th of November 2024
+###########################
+
+### introduction and overview over Spyder
+
+
+### basic datatypes: strings, integers, floats, print function
+
+# there are 2 ways to create strings:
+
+
+# ... and both can be used to create multi line strings with linebreaks
+
+
+# integers = numbers without decimals
+
+
+# floats = floating point numbers = numbers with decimals
+
+
+# the print() function is used to print output and results to the console
+
+
+# there are functions to convert datatypes into each other
+
+
+# Exercise 1
+
+### operators: +, -, /, *, **, //, %
+
+
+# Exercise 2
+
+### variables, string formatting
+
+# variables are placeholders for all kinds of datatypes
+# variable names should be explicit and informative
+
+
+# multiple variables can be defined in one line
+
+
+# string formatting is used to create dynamic strings and there are multiple
+# options
+# string formatting option 1
+
+
+# string formatting option 2
+
+
+# string formatting option 3
+
+
+# typical use case for string formatting: file path creation
+# here we use a "fromatted raw" string so that the backslashes are ignored
+
+
+### datatypes: lists, tuples, dictionaries, indices, exceptions / errors
+
+# lists are mutable / editable collections of all kinds of objects
+# lists and tuples are ordered and elements have an index, starting with 0
+# index          0         1           2
+
+# to call specific elements of a list or tuple, use the index behind the list
+
+
+# lists can be modified e.g. by adding or inserting values
+
+
+# len() gives us the number of elements of a list, tuple or string
+
+
+# range() by default creates a range of numbers starting from 0 up to the
+# entered number at a step of 1
+
+
+# lists can be multiplied
+
+
+# lists can be sliced up with indices
+
+
+# a dictionary is an unordered collection of pairs of keys and values
+# keys must be unique, values can be anything
+#              keys          values
+
+
+# new entries can be added to the dictionary like this
+
+
+# Exercise 3
