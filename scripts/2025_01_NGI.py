6df83efedd1d1c09a0006427424fbdd9c2ca4219
Sjur Beyer <sjur.beyer@ngi.no>
2025-05-05 00:22:12 +0200

    prep for the first course

---
+++ scripts/2025_01_NGI.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+"""
+Script for the "Python basics for geoscience and geotechnics" course
+from the Norwegian Geotechnical Institute. The course is held in May 2025 in 4x4 hour sessions.
+
+This script contains the code that was written during the first session
+The code is for educational purposes only.
+All content of the repository falls under the MIT-license -> see license file.
+
+Author: Dr. Georg H. Erharter, georg.erharter@ngi.no
+Modifications: Sjur Beyer, sjur.beyer@ngi.no
+"""
+
+###########################
+# session 1 on 5th of May 2025
+###########################
+
+### introduction and overview over Spyder
+
+
+### basic datatypes: strings, integers, floats, print function
+
+# Datatypes
+
+# integer numbers are represented with an "int" data type
+
+
+# decimal numbers are represented with a "float" data type
+
+
+# text is represented with a "str" data type
+# "str" is short for string, or string of characters
+# you may use either double quotes, ", single quotes, ', or
+# tripled double or single quotes, """ / ''', to construct
+# a string
+
+
+# You can print the data to the console by using the print() function
+
+
+# You may check the datatype by using the type() function, if you want
+# to see the results, you have to print it out
+
+
+
+
+
+# you can change the data type by using the int(), float() and str()
+# functions. Data might get lost in the process
+
+
+
+# Exercise 1
+
+
+# Exercise 2
+
+### variables, string formatting
+
+
+# Variables can be assigned data by setting them equal to a value
+# python automatically assigns a data type to the variable
+
+
+# You may also assign multiple variables respectively using comma
+
+
+# variable names should be explicit and informative
+
+
+
+# There are multiple ways to format a string
+
+# string concatenation: "string" + "another string"
+
+
+# .format() function replaces the {} with the function inputs
+
+
+# The string may be stored as a variable. We can add multiple inputs
+#  to .format()
+
+# f-strings are assigned by adding the character f before the string
+# These are useful for having a good idea about what you're printing
+
+
+# formatting strings is useful when working with or saving multiple images.
+# Here we can use some info to give file names to our images.
+
+
+### datatypes: lists, tuples, dictionaries, indices, exceptions / errors
+
+# The list is defined using square brackets
+
+
+# You may index a list by using integers starting at 0
+
+
+# You may add data to the list
+
+
+# You can find the amount of entries of a list using len()
+
+
+# You can slice a list by using colons, it returns a list
+# from (and including) the index 0 to, but not including
+# index 2
+
+
+# Tuples act similarly to lists in many ways, but cannot change
+# after assignment
+
+
+
+# Dictionaries store data in an unstructured way
+# These are indexed using keys (strings)
+
+# Dictionaries are defined by using curly brackets
+# Key and value pairs are separated by colon, entries
+# are separated with a comma. The values can be any data type
+
+
+
+
+# The values of a dictionary do not need to be lists
+# they can be any data type, like strings
+
+
+
+# range() by default creates a range of numbers starting from 0 up to the
+# entered number at a step of 1
+
+
+# you can set the starting number and the step size similarly
+# to how we slice lists (start, stop, step)
+
+
+# Exercise 3
+
+# Exercise 4
