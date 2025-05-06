d704917aa05d27c78c4a95264a012a0cc38afe65
Sjur Beyer <sjur.beyer@ngi.no>
2025-05-06 10:08:36 +0200

    Prep for the second session

---
+++ scripts/2025_02_NGI.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Script for the "Python basics for geoscience and geotechnics (Pilot course)"
+from the Norwegian Geotechnical Institute. The course is held in May 2025 in 4x4 hour sessions.
+
+This script contains the code that was written during the second session
+The code is for educational purposes only.
+All content of the repository falls under the MIT-license -> see license file.
+
+Author: Dr. Georg H. Erharter, georg.erharter@ngi.no
+Modifications: Sjur Beyer, sjur.beyer@ngi.no
+"""
+
+###########################
+# session 2 on 7th of May 2025
+###########################
+
+# repetition
+
+
+### control structures: conditional statements: if, elif, else, match cases,
+### operators
+
+# control structures help to either avoid or repeat certain parts of code
+# conditional statements make use of operators to compare if certain conditions
+# are True or False
+
+
+
+# these operators can be used in logical statements
+
+
+# elif can be used for additional tests NB! only if the first if statement
+# is false
+
+# match-cases are a new alternative to check if a variables matches something (==)
+
+
+### control structures: loops: while loop, for loop
+# loops are used to repeat parts of code
+
+# while loop is a loop that runs until a condition is not True anymore
+
+
+
+# Exercise 5
+
+
+
+# a while loop can be used to iterate over a list
+
+
+
+
+# # a better solution is however to use a for loop which is always finite
+
+
+# for i in range(len(...)): is a typical construction to get an index
+
+
+# Instead of iterating through indices, we may also iterate through
+# the list elements
+
+
+# The enumerate() function can be used to get both an element and the index
+
+
+# a "list comprehension" is a 1 line for loop as an alternative to a normal
+# for loop
+
+
+# it is short but comes at the price of readability
+
+### Boolean operators: or, and, not
+# These are used to do operations on True/False data, i.e boolean data
+
+
+
+# Exercise 6
+
+
+
+# Exercise 7
+
+
+
+
+
