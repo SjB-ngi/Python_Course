cb41887a8c0cf39ef44ef080a3e215b4d3a5425c
Sjur Beyer <sjur.beyer@ngi.no>
2025-05-07 13:28:38 +0200

    Adding empty script for the third session

---
+++ scripts/2025_03_NGI.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Script for the "Python basics for geoscience and geotechnics"
+from the Norwegian Geotechnical Institute. The course is held in May 2025 in 4x4 hour sessions.
+
+This script contains the code that was written during the third session
+The code is for educational purposes only.
+All content of the repository falls under the MIT-license -> see license file.
+
+Author: Dr. Georg H. Erharter, georg.erharter@ngi.no
+Modifications: Sjur Beyer, sjur.beyer@ngi.no
+"""
+
+
+###########################
+# session 3 on 12th of May 2025
+###########################
+
+# repetition
+
+
+# Exercise 3 - bonus
+
+
+
+
+# Exercise 8
+
+
+
+
+# Exercise 9
+
+
+
+### functions
+
+# Functions may be used to carry out a block of code whenever called. A function
+# is defined by using the def keyword followed by the function name with the input
+# argument(s) assigned inside the parenthesis. The value returned from the function
+# is defined using the return keyword
+
+
+
+# It is useful to assign data types to the inputs, which is done by separating
+# the input argument name and it's corresponding data type by colon in the 
+# function definition. The data type of the return value is given after the ->
+# symbol.
+
+
+# We can set default values to inputs by specifying them after the equals sign
+# in the function definition.
+
+
+# Setting default values means that we do not need to pass all the inputs to 
+# the function.
+
+
+
+
+# Exercise 10
+
+
