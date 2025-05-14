0f668a87e23cf469745249dfc9e4f3729f21eb8f
Sjur Beyer <sjur.beyer@ngi.no>
2025-05-14 07:16:25 +0200

    Adding empty scripts for the final session

---
+++ scripts/2025_04_NGI.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+"""
+Script for the "Python basics for geoscience and geotechnics"
+from the Norwegian Geotechnical Institute. The course is held in May 2025 in 4x4 hour sessions.
+
+This script contains the code that was written during the fourth session
+The code is for educational purposes only.
+All content of the repository falls under the MIT-license -> see license file.
+
+Author: Dr. Georg H. Erharter, georg.erharter@ngi.no
+Modifications: Sjur Beyer, sjur.beyer@ngi.no
+"""
+
+###########################
+# session 4 on 14th of May 2025
+###########################
+
+# repetition
+
+# importing scripts + coding style
+
+
+# There are multiple ways to import functions from another script
+# NOTE: The script must be in the same folder as the script you're using
+
+
+
+
+### modules, code environments, module documentation
+# Modules consist of python code written by others in the python community.
+# Using modules lets us do more with python without having to implement the
+# Functionality ourselves.
+
+# All modules, or code we make for others to use, should be thoroughly
+# documented. When you're using any community module you can check the
+# documentation to find out how the module works
+
+# Code environments hold collections of modules you have installed. In this
+# course we have been using Anaconda, which provides a nice overview of your
+# environments in the Anaconda Navigator.
+
+# numpy works based on arrays and is much faster than classical loops
+# The norm is to import numpy using the short name "np"
+
+
+
+# We can turn the number list into an array by using np.array()
+
+
+
+# The array can be sliced by using indices. The colon symbol : retrieves all
+# values for the axis which it is used.
+
+# the shape of the array can be queried and individual columns / rows accessed
+
+
+# We can use the array to do numerical operations such as summing all elements
+
+
+# We can also sum along specified axes. Using axis=0 sums the elements of all
+# rows, whilst using axis=1 sums the elements of all columns
+
+
+
+# numpy has many functions for mathematical oreations, these functions will
+# work element-wise over arrays of matching shapes
+
+# Say we want to sum the elements of two lists. Lets explore how this is done
+# first with pure python using loops, then with numpy
+
+
+
+# loop version
+
+
+
+# numpy version
+
+
+# Numerical operators in python (+, -, *, /, //, %) work element-wise on arrays
+
+# Exercise 11
+
+
+
+
+# numpy functions are highly optimized and faster than pure python code
+# this means that operations done on large amounts of data can be substantially
+# quicker using numpy
+
+
+### data handling with pandas, data visualization with matplotlib
+
+# pandas lets us load data in certain standardized formats, such as comma separated
+# values (.csv) and excel files (.xlsx). This data is loaded as the standard
+# data type in pandas, the pandas.DataFrame
+
+
+# We install the openly available PremstallerGeotechnik dataset, which contains
+# CPT data from various sites in Austria and Germany.
+# https://www.tugraz.at/en/institutes/ibg/research/computational-geotechnics-group/database
+
+
+# We unzip the data into a folder called "data" in our working directory. This
+# lets us specify an easy file path to the unzipped .csv file
+
+
+# We read the CPT data into a pandas.DataFrame using the pd.read_csv() function
+
+
+# Every dataframe has a header which we can get an overview of using .columns
+
+
+# pandas dataframes are indexed using a header key, which is similar to how
+# dictionaries are indexed. The returned output when indexing a pandas dataframe
+# is all of the values corresponding to the column belonging to the key
+
+# pandas dataframes are structured like a single spreadsheet, so we can also
+# access the value at a given row index using .loc
+
+
+# We can also use logical indexing, where we specify a condition in square
+# brackets after the dataframe indicating which values we want to retrieve
+
+
+# We can get basic statistics of dataframe using .describe()
+
+
+# Since the terminal truncates the output due to the large amount of rows and
+# columns, we can more easily inspect the result of .describe() by writing the
+# results to an excel file using the .to_excel() function.
+
+
+# If we want to group the data in different subsets, we can use the .groupby()
+# function. Here we specify a header key for which all unique elements will
+# be grouped. The output of .groupby() is a collection of tuples with the value
+# used for grouping and the subset where the column holds that value
+
+
+# Exercise 12
+
+
+
+# Exercise 13
+
+
+
+# Field Manager API
+# If you're interested in using the python API for the NGI developed platform Field Manager
+# please contact either me, or the field manager team
+# https://www.fieldmanager.io/support
---
+++ scripts/python_course_utility.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+"""
+Python file containing functions to be used in the "Python basics for
+geoscience and geotechnics" by the Norwegian Geotechnical Institute
+
+@author: Sjur Beyer, sjur.beyer@ngi.no
+"""
+
+
+import time
+
+
+def custom_mean(input_list: list) -> float:
+    """
+    Computes the mean using pure python functions
+    """
+    return sum(input_list) / len(input_list)
+
+
+def custom_median(input_list: list) -> float:
+    """
+    Computes the median of a list of numerical values using only
+    pure Python functions.
+
+    The median is the middle value in a sorted list.
+    If the list has an even number of elements, the median is the average
+    of the two middle elements.
+
+    Parameters
+    ----------
+    input_list : list
+        list containing numerical values.
+
+    Returns
+    -------
+    median : float
+        The median of the input list, returned as a float.
+
+    Notes
+    -----
+    - The input list must contain only numerical values
+    - The function does not handle empty lists
+
+    """
+
+    input_sorted = sorted(input_list)
+
+    if len(input_list) % 2 == 0:
+        upper_val = input_sorted[len(input_sorted)//2]
+        lower_val = input_sorted[len(input_sorted)//2-1]
+        median = (upper_val + lower_val)/2
+    else:
+        median = input_sorted[len(input_sorted)//2-1]
+    return median
+
+
+def custom_variance(input_list: list) -> float:
+    """
+    Computes the variance of a list of numerical values using pure Python functions.
+
+    The variance is a measure of how far the elements of the list are spread out from the mean. 
+    It is calculated as the average of the squared differences between each element and the mean.
+
+    Parameters
+    ----------
+    input_list : list
+        A list of numerical values (int or float). The list must be non-empty.
+
+    Returns
+    -------
+    variance : float
+        The variance of the input list, returned as a float.
+
+    Notes
+    -----
+    - The input list must contain only numerical values.
+    - The function does not handle empty lists or non-numeric inputs.
+    - This implementation uses a helper function `custom_mean()` to compute the mean.
+    """
+    # Use the mean function to compute the mean
+    mean = custom_mean(input_list)
+
+    # Instantiate a temporary list
+    variance_temp = []
+
+    # Append the squared differences to the list
+    for c_i in input_list:
+        variance_temp.append((c_i-mean)**2)
+
+    # Computing variance as the mean of the squared differences
+    # Here we could also use the custom_mean() function if we want
+    variance = 1/len(input_list) * sum(variance_temp)
+
+    return variance
+
+
+def custom_std(input_list: list) -> float:
+    """
+
+
+    Parameters
+    ----------
+    input_list : list
+        DESCRIPTION.
+
+    Returns
+    -------
+    float
+        DESCRIPTION.
+
+    """
+    variance = custom_variance(input_list)
+    return variance**(1/2)
+
+
+def profile_function(func):  # No type hints
+    """
+    A decorator used for timing functions
+
+    """
+    def wrapper(*args, **kwargs):
+        start_time = time.time()        # Record start time
+        result = func(*args, **kwargs)  # Call the original function
+        end_time = time.time()          # Record end time
+        elapsed_time = end_time - start_time  # Calculate elapsed time
+        # Choose the appropriate unit
+        if elapsed_time < 1e-9:
+            time_unit = "ps"  # picoseconds
+            nice_time = elapsed_time*1e12
+        elif elapsed_time < 1e-6:
+            time_unit = "ns"
+            nice_time = elapsed_time * 1e9
+        elif elapsed_time < 1e-3:
+            time_unit = "µs"
+            nice_time = elapsed_time * 1e6
+        elif elapsed_time < 1:
+            time_unit = "ms"
+            nice_time = elapsed_time * 1e3
+        else:
+            time_unit = "s"
+            nice_time = elapsed_time
+        print(f"Function '{func.__name__}' took {nice_time:.2f} {time_unit}\n")
+        return result                   # Return the original function's result
+    return wrapper
